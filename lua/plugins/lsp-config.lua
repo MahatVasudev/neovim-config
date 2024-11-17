753b9bbf0c448cf3cecaf06592964412ea78837f
SomeOneWho <105697151+MahatVasudev@users.noreply.github.com>
2024-11-17 19:20:25 +0530

    Adding More LSP features

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,7 +14,7 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
-		lazy = false,
+     lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
@@ -50,6 +50,10 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<Leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+
+      vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, {})
+    end,
+	}, 
+        
 }
